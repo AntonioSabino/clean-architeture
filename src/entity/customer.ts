583a3ee88830289fae0975546dc1735a017dafc8
Antonio Sabino <aslimajr@outlook.com>
2023-06-30 21:26:28 -0300

    adicionando Value Object Address

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,37 +1,43 @@
+import { Address } from './address'
+
 class Customer {
-  _id: string;
-  _name: string = '';
-  _address: string = '';
-  _active: boolean = false;
+  _id: string
+  _name: string = ''
+  _address!: Address
+  _active: boolean = false
 
   constructor(id: string, name: string) {
-    this._id = id;
-    this._name = name;
-    this.validate();
+    this._id = id
+    this._name = name
+    this.validate()
   }
 
   validate(): void {
     if (this._name.length === 0) {
-      throw new Error('Name must be at least 5 characters long');
+      throw new Error('Name must be at least 5 characters long')
     }
     if (this._id.length === 0) {
-      throw new Error('Id is required');
+      throw new Error('Id is required')
     }
   }
 
   changeName(name: string): void {
-    this._name = name;
-    this.validate();
+    this._name = name
+    this.validate()
   }
 
   activate(): void {
-    if(this._address.length === 0) {
-      throw new Error('Address is required');
+    if (this._address === undefined) {
+      throw new Error('Address is required')
     }
-    this._active = true;
+    this._active = true
   }
 
   desactivate(): void {
-    this._active = false;
+    this._active = false
+  }
+
+  set address(address: Address) {
+    this._address = address
   }
 }
