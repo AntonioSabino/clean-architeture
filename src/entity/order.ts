8edf9b797c1c1821f90ecccfb6709f86fed35282
Antonio Sabino <aslimajr@outlook.com>
2024-01-20 11:54:34 -0300

    relacionamento Item com Product

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,32 +1,35 @@
-import OrderItem from "./order_item"
+import OrderItem from './order_item'
 
 export default class Order {
-  private _id: string
-  private _customerId: string
-  private _items: OrderItem[]
-  private _total: number
+	private _id: string
+	private _customerId: string
+	private _items: OrderItem[]
+	private _total: number
 
-  constructor(id: string, customerId: string, items: OrderItem[] = []) {
-    this._id = id
-    this._customerId = customerId
-    this._items = items
-    this._total = this.calculateTotal()
-    this.validate()
-  }
+	constructor(id: string, customerId: string, items: OrderItem[] = []) {
+		this._id = id
+		this._customerId = customerId
+		this._items = items
+		this._total = this.calculateTotal()
+		this.validate()
+	}
 
-  validate(): void {
-    if (this._id.length === 0) {
-      throw new Error('Id is required')
-    }
-    if (this._customerId.length === 0) {
-      throw new Error('CustomerId is required')
-    }
-    if (this._items.length === 0) {
-      throw new Error('Items are required')
-    }
-  }
+	validate(): void {
+		if (this._id.length === 0) {
+			throw new Error('Id is required')
+		}
+		if (this._customerId.length === 0) {
+			throw new Error('CustomerId is required')
+		}
+		if (this._items.length === 0) {
+			throw new Error('Items are required')
+		}
+		if (this._items.some((item) => item.quantity < 1)) {
+			throw new Error('Item quantity must be greater than 0')
+		}
+	}
 
-  calculateTotal(): number {
-    return this._items.reduce((total, item) => total + item.price, 0)
-  }
+	calculateTotal(): number {
+		return this._items.reduce((total, item) => total + item.price, 0)
+	}
 }
