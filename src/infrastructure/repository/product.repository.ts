c15327b5a2286f1da88f12f55700578b53796e8e
Antonio Sabino <aslimajr@outlook.com>
2024-01-22 22:17:04 -0300

    implementando e testando product repository

---
+++ src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,50 @@
+import Product from '../../domain/entity/product'
+import ProductRepositoryInterface from '../../domain/repository/product.repository.interface'
+import ProductModel from '../db/sequelize/model/product.model'
+
+export default class ProductRepository implements ProductRepositoryInterface {
+	async create(product: Product): Promise<void> {
+		await ProductModel.create({
+			id: product.id,
+			name: product.name,
+			price: product.price,
+		})
+	}
+
+	async update(product: Product): Promise<void> {
+		await ProductModel.update(
+			{
+				name: product.name,
+				price: product.price,
+			},
+			{ where: { id: product.id } }
+		)
+	}
+
+	async find(id: string): Promise<Product> {
+		const productModel = await ProductModel.findOne({ where: { id } })
+
+		if (!productModel) {
+			throw new Error('Product not found')
+		}
+
+		return new Product(
+			productModel.getDataValue('id'),
+			productModel.getDataValue('name'),
+			productModel.getDataValue('price')
+		)
+	}
+
+	async findAll(): Promise<Product[]> {
+		const productModels = await ProductModel.findAll()
+
+		return productModels.map(
+			(productModel) =>
+				new Product(
+					productModel.getDataValue('id'),
+					productModel.getDataValue('name'),
+					productModel.getDataValue('price')
+				)
+		)
+	}
+}
